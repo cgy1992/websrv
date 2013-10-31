a3a55071dd0de9c2c016964553c580c4a14602c9
Сельченков Николай <undwad@integra-s.com>
2013-10-31 14:27:32 +0400

    +

--- output/test.lua
+++ output/test.lua
@@ -5,21 +5,15 @@ pprint = function(arg) print(ptostring(arg)) end
 
 require 'websrv'
 
-local server = websrv.server.init{port = 80, file = 'help.log'}
+local server = websrv.server.init{port = 80, file = 'help.cfg', flags = websrv.flags.USEEXTCONF}
 websrv.server.addhandler{server = server, mstr = '* *', func = function(session)
-	local txt = {"one", "two", "three", "four", "five"}
 	session.write("Content-type: text/html\r\n\r\n")
-	session.write("<form method='QUERY'>\r\n")
-	for i = 1, 5 do
-		session.write("<input type='checkbox' name='number' value='"..txt[i].."'>\r\n<BR>")
-	end
-	session.write("<input type='submit' name='send' value=' SEND '>\r\n<BR>")
-	session.write("</form>\r\n")
-	session.write("You have choosen <font color='FF0000'>"..session.Query("number").."</font> numbers: \r\n")
-	for i = 1, session.Query("#number") do
-		session.write("<b>"..session.Query("number").."</b>,\r\n\r\n")
-	end
-	session.write("...<BR>\r\n\r\n")
+	session.write("<PRE>")
+	websrv.client.addfile('help.cfg')
+	session.write("</PRE>")
+	session.write("ClientInfo->Conf(\"PERSONAL_CONF\",\"PORT\")="..session.Conf("PERSONAL_CONF","PORT").."<BR>\n")
+	session.write("ClientInfo->Conf(\"PERSONAL_CONF\",\"IP\")="..session.Conf("PERSONAL_CONF","IP").."<BR>\n")
+	session.write("ClientInfo->Conf(\"LIBWEBSERVER\",\"PORT\")="..session.Conf("LIBWEBSERVER","PORT").."<BR>\n")
 end}
 
 while true do
