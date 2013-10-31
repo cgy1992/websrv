79e19c170603555b28b9e456537a50eec953fc86
Сельченков Николай <undwad@integra-s.com>
2013-10-31 12:07:42 +0400

    +

--- output/test.lua
+++ output/test.lua
@@ -7,18 +7,15 @@ require 'websrv'
 
 local server = websrv.server.init{port = 80, file = 'help.log'}
 websrv.server.addhandler{server = server, mstr = '* *', func = function(session)
-	if 'username' ~= session.username or 'password' ~= session.password then
-		websrv.client.HTTPdirective('HTTP/1.1 401 Authorization Required')
-        session.write('WWW-Authenticate: Basic realm=\"This site info\"\r\n')
-        session.write('Content-type: text/html\r\n\r\n')
-        session.write('\n')
-        session.write('Access denied\n')
-        session.write('\n')
+	local username = session.Post('user')
+	if username then
+		websrv.client.setcookie{key = 'username', value = username, timeoffset = '+15V'}
 	end
-    session.write('Content-type: text/html\r\n\r\n')         
-    session.write('\n')        
-    session.write('You entered in your area\n')
-    session.write('\n')
+	session.write("Content-type: text/html\r\n\r\n")
+	session.write("<form method='POST'>\r\n")
+	session.write("<input type='text' name='user' value='"..session.Cookie('username').."'>\r\n<BR>")
+    session.write("<input type='submit' name='send' value=' GO! '>\r\n<BR>")
+    session.write("</form>\r\n")
 end}
 
 while true do
