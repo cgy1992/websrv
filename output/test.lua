5f57b08dca89988af600dccdd7866775b016a7ed
Сельченков Николай <undwad@integra-s.com>
2013-10-31 16:42:49 +0400

    +

--- output/test.lua
+++ output/test.lua
@@ -5,25 +5,27 @@ pprint = function(arg) print(ptostring(arg)) end
 
 require 'websrv'
 
+local buffer = require 'buffer'
+
 local GIFSIDE = 320
-local gifdata = string.rep('\0', GIFSIDE * GIFSIDE)
+local gifdata = buffer(GIFSIDE * GIFSIDE)
 local server = websrv.server.init{port = 80, file = 'help.log'}
 websrv.server.addhandler{server = server, mstr = '* *', func = function(session)
 	websrv.client.gifsetpalette("EGA")
 	local img = session.Query("img")
-	if img then
-		session.write("Content-type: image/gif\r\n\r\n")
+	if img and string.len(img) > 0 then
 		if 'circle' == img then
+			session.write("Content-type: image/gif\r\n\r\n")
 			local xc = tonumber(session.Query('x')) % GIFSIDE
 			local yc = tonumber(session.Query('y')) % GIFSIDE
 			local color = (math.random() % 15) + 1
 			for i = 0, 6.28, 0.01 do
-				local x = (GIFSIDE + (xc + math.cos(i) * 10)) % GIFSIDE
-				local y = (GIFSIDE + (yc + math.sin(i) * 10)) % GIFSIDE
-				gifdata[x + (y * GIFSIDE)] = color
+				local x = (GIFSIDE + math.round((xc + math.cos(i) * 10))) % GIFSIDE
+				local y = (GIFSIDE + math.round((yc + math.sin(i) * 10))) % GIFSIDE
+				gifdata[1 + x + (y * GIFSIDE)] = color
 			end
 		end
-		websrv.client.gifoutput{data = gifdata, width = GIFSIDE, height = GIFSIDE}
+		websrv.client.gifoutput{data = gifdata:tostring(), width = GIFSIDE, height = GIFSIDE}
 	end
 	session.write('Content-type: text/html\r\n\r\n')
 	session.write("<center>Generated a circle (click inside the image)<BR>\n")
