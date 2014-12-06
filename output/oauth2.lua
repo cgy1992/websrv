1021cad9752299a0ce4307e323b25a6593260ff4
Сельченков Николай <undwad@integra-s.com>
2014-12-06 17:32:15 +0400

    +

--- output/oauth2.lua
+++ output/oauth2.lua
@@ -7,7 +7,6 @@ local ltn12 = require "ltn12"
 local json = require 'json'
 
 local folder = 'f:/github/websrv/output/'
-local states = {}
 
 function string:replace(map)
     local result = self
@@ -42,7 +41,7 @@ local oauth2 =
         yandex =
         {
             enabled = true,
-            href_pattern = 'https://oauth.yandex.ru/authorize?response_type=code&client_id={CLIENT_ID}&state={STATE}',
+            href_pattern = 'https://oauth.yandex.ru/authorize?response_type=code&client_id={CLIENT_ID}',
             client_id = 'd2f8ddcb159d4da1b26987c686e98409',
             client_secret = 'be22fb5f63ec451caf505020cf42527e',
             access_token_uri = 'https://oauth.yandex.ru/token',
@@ -51,7 +50,7 @@ local oauth2 =
         google =
         {
             enabled = true,
-            href_pattern = 'https://accounts.google.com/o/oauth2/auth?redirect_uri={REDIRECT_URI}&response_type=code&client_id={CLIENT_ID}&scope=email&state={STATE}',
+            href_pattern = 'https://accounts.google.com/o/oauth2/auth?redirect_uri={REDIRECT_URI}&response_type=code&client_id={CLIENT_ID}&scope=email',
             client_id = '1003457679327-gu20cis8v037ul2jvdi5rtg71mvf9qsg.apps.googleusercontent.com',
             client_secret = '0322hnJ9OIb8ZwRzyjk35i_W',
             access_token_uri = 'https://accounts.google.com/o/oauth2/token',
@@ -60,12 +59,13 @@ local oauth2 =
         facebook =
         {
             enabled = false,
-            href_pattern = 'https://www.facebook.com/dialog/oauth?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code&state={STATE}',
+            href_pattern = 'https://www.facebook.com/dialog/oauth?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code',
             client_id = '740409662718765',
             client_secret = 'f8df3185f4c62b719a8acbaffe1d8d26',
             access_token_uri = 'https://accounts.google.com/o/oauth2/token',
         }
-    }
+    },
+    sessions = {}
 }
 
 local server = websrv.server.init{port = 443, file = folder..'test.log', flags = websrv.flags.USESSL, cert = folder..'foo-cert.pem'}
@@ -78,62 +78,82 @@ local function return_error(session, code, text)
     session.write('\n\n')        
 end
 
+local function return_redirect(session, to)
+    session.write('Content-type: text/html\r\n\r\n')
+    session.write('<script language="JavaScript" type="text/javascript">location.href="'..to..'"</script>')
+end
+
+local function return_args(session, ...)
+    session.write('Content-type: text/html\r\n\r\n')
+    for i,arg in ipairs{...} do
+        session.write(tostring(arg))
+    end
+end
+
 websrv.server.addhandler{server = server, mstr = '*/login', func = function(session)
-    local state = session.Query("state")
-    if string.len(state) > 0 then
-        session.write 'Content-type: text/html\r\n\r\n'
-        session.write '<HTML><BODY bgcolor="EFEFEF">'
-        for service, params in pairs(oauth2.services) do
-            if params.enabled then
-                local href = string.replace(params.href_pattern, {
-                    ['{REDIRECT_URI}'] = url.escape(oauth2.redirect_uri..service), 
-                    ['{CLIENT_ID}'] = params.client_id,
-                    ['{STATE}'] = state,
-                })
-                session.write('<a href="'..href..'" title="enter via '..service..'">enter via '..service..'</a><br/>')
-            end
+    session.write 'Content-type: text/html\r\n\r\n'
+    session.write '<HTML><BODY bgcolor="EFEFEF">'
+    for service, params in pairs(oauth2.services) do
+        if params.enabled then
+            session.write('<a href="/login/'..service..'" title="enter via '..service..'">enter via '..service..'</a><br/>')
         end
-        states[state] = {}
+    end
+end}
+
+websrv.server.addhandler{server = server, mstr = '*/login/*', func = function(session)
+    local path = url.parse_path(session.request)
+    local service = path[#path]
+    local params = oauth2.services[service]
+    if params and params.enabled then
+        local href = string.replace(params.href_pattern, {
+            ['{REDIRECT_URI}'] = url.escape(oauth2.redirect_uri..service), 
+            ['{CLIENT_ID}'] = params.client_id
+        })
+        return_redirect(session, href)
     else return_error(session, 400, 'Bad Request') end
 end}
 
 websrv.server.addhandler{server = server, mstr = '*/oauth2/*', func = function(session)
     local path = url.parse_path(session.request)
     local service = path[#path]
-    local state = session.Query("state")
     local code = session.Query("code")
-    if oauth2.services[service] and string.len(state) > 0 and string.len(code) > 0 then
-        if states[state] then
-            local params = oauth2.services[service]
-
-            local body = string.replace(params.access_token_body_pattern or oauth2.access_token_body_pattern, {
-                ['{REDIRECT_URI}'] = url.escape(oauth2.redirect_uri..service), 
-                ['{CLIENT_ID}'] = params.client_id,
-                ['{CODE}'] = url.escape(code),
-                ['{CLIENT_SECRET}'] = params.client_secret,
-            })
+    local params = oauth2.services[service]
+    if params and params.enabled and string.len(code) > 0 then
+        local body = string.replace(params.access_token_body_pattern or oauth2.access_token_body_pattern, {
+            ['{REDIRECT_URI}'] = url.escape(oauth2.redirect_uri..service), 
+            ['{CLIENT_ID}'] = params.client_id,
+            ['{CODE}'] = url.escape(code),
+            ['{CLIENT_SECRET}'] = params.client_secret,
+        })
+        
+        local res, code, headers, status = ssl.https.request(params.access_token_uri, body)
+        
+        local oauth = json:decode(res)
             
-            local res, code, headers, status = ssl.https.request(params.access_token_uri, body)
-            
-            local oauth = json:decode(res)
-                
+        if 200 == code then
+            local response = {} 
+            local res, code, headers, status = ssl.https.request({
+                url = params.info_uri,
+                headers = { Authorization = "Bearer "..oauth.access_token },
+                sink = ltn12.sink.table(response),
+            })                  
             if 200 == code then
-                local response = {} 
-                local res, code, headers, status = ssl.https.request({
-                    url = params.info_uri,
-                    headers = { Authorization = "Bearer "..oauth.access_token },
-                    sink = ltn12.sink.table(response),
-                })                  
-                if 200 == code then
-                    local info = json:decode(response[1])
-                    session.write 'Content-type: text/plain\r\n\r\n'
-                    session.write(tostring(info)..'\n\n')
-                    session.write(state)
-                    states[state] = info
-                else return_error(session, 401, 'Unauthorized') end
-            else return_error(session, code, res.error_description) end
-        else return_error(session, 401, 'Unauthorized') end
+                local info = json:decode(response[1])
+                info.expires_in = oauth.expires_in
+                oauth2.sessions[oauth.access_token] = info
+                return_redirect(session, '/info?access_token='..oauth.access_token)
+            else return_error(session, 401, 'Unauthorized') end
+        else return_error(session, code, res.error_description) end
     else return_error(session, 400, 'Bad Request') end
+end}
+
+websrv.server.addhandler{server = server, mstr = '*/info', func = function(session)
+    local access_token = session.Query("access_token")
+    local info = oauth2.sessions[access_token]
+    if info then
+        session.write 'Content-type: text/html\r\n\r\n'
+        session.write(tostring(info))
+    else return_error(session, 401, 'Unauthorized') end
 end}
 
 websrv.server.addhandler{server = server, mstr = '*/check', func = function(session)
